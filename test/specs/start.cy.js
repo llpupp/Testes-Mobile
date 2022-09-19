e27ce277c2c82f0e56c86457d422a7a71c459d99
Luciano Pupp <llpupp@hotmail.com>
2022-09-19 14:09:10 -0300

    Inclusão da opão Dropdown

--- test/specs/start.cy.js
+++ test/specs/start.cy.js
@@ -4,7 +4,11 @@ describe('Primeiro teste', () => {
        await $('~text-input').setValue('Teste')
        expect(await $ ('~text-input')).toBeDisplayed()
        
-       
+       await $('~Dropdown').click()
+        const selector = 'new UiSelector().text("This app is awesome").className("android.widget.CheckedTextView")'
+        const button = await $(`android=${selector}`)
+        await button.click()
+
        
         
     })
